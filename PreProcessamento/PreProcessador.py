ff8cb613f808b328b37d9ce5fae91822bb9839a6
Elias Cyrino <eliascyrino@gmail.com>
2020-08-21 16:36:03 -0300

    funçao separar silabas adicionadas

--- PreProcessamento/PreProcessador.py
+++ PreProcessamento/PreProcessador.py
@@ -6,22 +6,16 @@ arq = open("brazilian", 'r')
 palavras = arq.read().split('\n')
 arq.close()
 nova_lista_palavras = []
-print(len(palavras))
 
 # Limpando as palavras da lista e gravando os dados em um arquivo csv
 #with open("palavrasProcessadas.csv",'w') as arq:
     #arq.write("palavra,canonicidade,tonicidade\n")
-    # Filtra as palavras desejadas (sem letras maiúsculas, sem hífen e com o mínimo de 3 letras)
+# Filtra as palavras desejadas (sem letras maiúsculas, sem hífen e com o mínimo de 3 letras)
 for p in palavras:
     if not fa.maiuscula(p):
         if fa.min_threshold_letras(3, p):
             if not fa.hifen(p): 
-                if not fa.cz_ps(p):
-                     if not fa.tres_consoantes(p):
-                          if not fa.prefixos(p):
-                                if not fa.hiato_final_palavra(p):
-                                    if not fa.consoante_consoante_m(p,1):
-                                        if fa.validar_formato(p):
-                                            nova_lista_palavras.append(p)
-                                            
-print(len(nova_lista_palavras))
+                if not fa.prefixos(p):
+                    if not fa.hiato_final_palavra(p):
+                        if fa.validar_formato(p):
+                            nova_lista_palavras.append(p)
--- PreProcessamento/funcoesAuxiliares.py
+++ PreProcessamento/funcoesAuxiliares.py
@@ -1,7 +1,79 @@
-# Imports
+## Imports
 import re
 
-# Contém as funções auxiliares para a leitura das palavras desejadas
+## Variáveis globais utilizadas na separação de sílabas e identificação de formatos
+# Tritongos
+tritongos = ['uai','uei','uou','uiu','uão','uem','uõe']
+formatos_tritongo = [
+                        'VCVCVVVC','VCVCVVVV','VCVCVVV','CVCVCVVVV','CVCVCVVVC','CVCVCVVV',
+                        'CVCVVV','CVCVVVV','CVCVVVC','VCVVVC','VCVVVC','VCVVV'
+                    ]
+formatos_tritongo_hifenizados = [
+                                    'V-CV-CVVV-C','V-CV-CVVV-V','V-CV-CVVV','CV-CV-CVVV-V',
+                                    'CV-CV-CVVV-C','CV-CV-CVVV','CV-CVVV','CV-CVVV-V',
+                                    'CV-CVVV-C','V-CVVV-V','V-CVVV-C','V-CVVV'
+                                ]
+# Ditongos
+ditongos_decrescentes = [
+                                'ai','êi','ãi','ei','éi','oi','ói','ui','au','áu','eu',
+                                'éu','iu','ou','ãe','ão','õe'
+                            ]
+formatos_ditongo = ['CVCVVCV','VCVVCV','CVCVV','VCVV','CVVCV','CVVV','VVCV','CVCCVVV']
+formatos_ditongo_hifenizados = [
+                                'CV-CVV-CV','V-CVV-CV','CV-CVV','V-CVV','CVV-CV',
+                                'CVV-V','VV-CV','CVC-CVV-V'
+                               ]
+# Hiatos ou vogais duplas
+hiatos_vogais_duplas = [
+                        'ea','eo','ia','ie','io','oa','oe','ua','ue','ui','uo', # hiatos
+                        'aa','ee','ii','oo','uu' # vogais duplas
+                       ]
+formatos_hiatos_vogais_duplas = ['CVVCV','VVCV','CVCVVCV','CVV','CVCCVVVCV','CVCVVVCV']
+formatos_hiatos_vogais_duplas_hifenizados = [
+                                                'CV-V-CV','V-V-CV','CV-CV-V-CV','CV-V',
+                                                'CVC-CV-VV-CV','CV-CVV-V-CV'    
+                                            ]
+# Qu ou Gu
+padroes_gu_qu = ['gu','qu']
+formatos_gu_qu = [
+                    'CVCVVCV','CVCCVV','CVVCVCCV','VCVV','CVCVCCVV',
+                    'CVCCVVCV','CCVCCVVCV','VCVVCCCV'
+                 ]
+formatos_gu_qu_hifenizados = [
+                                'CV-CVV-CV','CVC-CVV','CVV-CVC-CV','V-CVV','CV-CVC-CVV',
+                                'CVC-CVV-CV','CCVC-CVV-CV','V-CVV-CCV'
+                             ]
+# Consoantes indivisíveis
+padroes_consoante_indivisivel = [
+                'br','bl','cr','cl','dr','fr','fl','gr','gl','pr','pl','tr','vr',
+                'lh','nh','ch'
+              ]
+formatos_consoante_indivisivel = [
+                                    'CCVCV','CVCCV','CVCVCCV','CCVCCV','VCCVCV',
+                                    'CCVVCCV','CVCCVCV','CVCCVC'
+                                 ]
+formatos_consoante_indivisivel_hifenizados = [
+                                                'CCV-CV','CV-CCV','CV-CV-CCV','CCV-CCV',
+                                                'V-CCV-CV','CCV-VC-CV','CV-CCV-CV','CV-CCVC'
+                                             ]
+# Cz ou ps
+padroes_cz_ps = ['cz','ps']
+formatos_cz_ps = ['CCVCVCV','CCVCCV','CCVCCV','CCVCVCCV','CCVCVCVCV','CCVCVCVCVV']
+formatos_cz_ps_hifenizados = [
+                                'CCV-CV-CV','CCVC-CV','CCV-CCV',
+                                'CCV-CVC-CV','CCV-CV-CV-CV	','CCV-CV-CV-CV-V'
+                             ]
+# Outros formatos
+outros_formatos = [
+                'CVCVC','VCV','VCCVCV','VCCVCVCVC','CVCCV','CVCCVCV','CVCCVCCV',
+                'CVVCCV','CVCCVC','CVCVCCV','CVCCVCVV','CVCCCVCVV','VCVCVCV'
+               ]
+outros_formatos_hifenizados = [
+                'CV-CVC','V-CV','VC-CV-CV','VC-CV-CV-CVC','CVC-CV','CVC-CV-CV','CVC-CVC-CV',
+                'CVVC-CV','CVC-CVC','CV-CVC-CV','CVC-CV-CV-V','CVC-CCV-CV-V','V-CV-CV-CV'
+               ]
+
+## Contém as funções auxiliares para a leitura das palavras desejadas
 def representacao_palavra(palavra):
     v_letras = ""
     for l in palavra:
@@ -55,19 +127,19 @@ def hiato_final_palavra(palavra):
                 return True
     return False
 
-def cz_ps(palavra):
+"""def cz_ps(palavra):
     padroes = ['cz','ps']
     for p in padroes:
         if re.search(p,palavra) is not None:
             return True
-    return False
+    return False"""
 
-def tres_consoantes(palavra):
+"""def tres_consoantes(palavra):
     v_letras = representacao_palavra(palavra)
     if re.search("CCC",v_letras)is not None:
         return True
     else:
-        return False
+        return False"""
 
 def prefixos(palavra):
     padroes = ["sub","ab","ad"]
@@ -81,13 +153,13 @@ def prefixos(palavra):
         else:
             return False
 
-def consoante_consoante_m(palavra,m):
+"""def consoante_consoante_m(palavra,m):
     v_letras = representacao_palavra(palavra)
     aux = len(re.findall("CC",palavra))
     if aux > m:
         return True
     else:
-        return False
+        return False"""
 
 def silaba_tem_vogal(silaba):
     vogais = "aeiouáéíóúâêôã"
@@ -98,82 +170,118 @@ def silaba_tem_vogal(silaba):
 
 # --------------------------------------------------------------------------------------------
 # Funções que definem os formatos das palavras que serão lidas
-def tem_tritongo_formato(palavra):
-    tritongos = ['uai','uei','uou','uiu','uão','uem','uõe']
-    formatos_tritongo = [
-                            'VCVCVVVC','VCVCVVVV','VCVCVVV','CVCVCVVVV','CVCVCVVVC','CVCVCVVV',
-                            'CVCVVV','CVCVVVV','CVCVVVC','VCVVVC','VCVVVC','VCVVV'
-                        ]
+def tem_tritongo(palavra):
     return verificar_formato(tritongos,formatos_tritongo,palavra)
 
-def tem_ditongo_formato(palavra):
-    ditongos_decrescentes = [
-                                'ai','êi','ãi','ei','éi','oi','ói','ui','au','áu','eu',
-                                'éu','iu','ou','ãe','ão','õe'
-                            ]
-    formatos_ditongo = ['CVCVVCV','VCVVCV','CVCVV','VCVV','CVVCV','CVV','VVCV','CVCCVVV']
+def tem_ditongo(palavra):
     return verificar_formato(ditongos_decrescentes,formatos_ditongo,palavra)
 
-def tem_hiato_vogais_duplas_formato(palavra):
-    hiatos_vogais_duplas = [
-                                'ea','eo','ia','ie','io','oa','oe','ua','ue','ui','uo', # hiatos
-                                'aa','ee','ii','oo','uu' # vogais duplas
-                           ]
-    formatos_hiatos_vogais_duplas = ['CVVCV','VVCV','CVCVVCV','CVV','CVCCVVVCV','CVCVVVCV']
+def tem_hiato_vogais_duplas(palavra):
     return verificar_formato(hiatos_vogais_duplas,formatos_hiatos_vogais_duplas,palavra)
 
 def tem_gu_qu(palavra):
-    padroes = ['gu','qu']
-    formatos_gu_qu = [
-                        'CVCVVCV','CVCCVV','CVVCVCCV','VCVV','CVCVCCVV',
-                        'CVCCVVCV','CCVCCVVCV','VCVVCCCV'
-                    ]
-    return verificar_formato(padroes,formatos_gu_qu,palavra)
+    return verificar_formato(padroes_gu_qu,formatos_gu_qu,palavra)
 
-def tem_bcdfgptv_lh_formato(palavra):
-    padroes = [
-                'br','bl','cr','cl','dr','fr','fl','gr','gl','pr','pl','tr','vr',
-                'lh','nh','ch'
-              ]
-    formatos_bcdfgptv_lh = [
-                            'CCVCV','CVCCV','CVCVCCV','CCVCCV','VCCVCV',
-                            'CCVVCCV','CVCCV','CVCCVCV'
-                           ]
-    return verificar_formato(padroes,formatos_bcdfgptv_lh,palavra)
+def tem_consoante_indivisivel(palavra):
+    return verificar_formato(padroes_consoante_indivisivel,formatos_consoante_indivisivel,palavra)
+
+def tem_cz_ps(palavra):
+    return verificar_formato(padroes_cz_ps,formatos_cz_ps,palavra)
 
 def tem_outros_formatos(palavra):
-    formatos = [
-                'CVCVC','VCV','VCCVCV','VCCVCVCVC','CVCCV','CVCCVCV','CVCCVCCV',
-                'CVVCCV','CVCCVC','CVCVCCV','CVCCVCVV','CVCCCVCVV'
-               ]
-    for formato in formatos:
+    count = 0
+    for formato in outros_formatos:
         if representacao_palavra(palavra) == formato:
-            return True
-    return False
+            return (True,count)
+        count += 1
+    return (False,None)
 
 def verificar_formato(padroes,formatos, palavra):
+    count = 0
     for padrao in padroes:
         if padrao in palavra:
             for formato in formatos:
                 if representacao_palavra(palavra) == formato:
-                    return True
-    return False
+                    return (True,count)
+                count += 1
+    return (False,None)
 
-# Definindo novo formato para as palavras não-canônicas
 def validar_formato(palavra):
     if canonicidade(palavra):
         return True
     else:
-        if tem_ditongo_formato(palavra):
+        if tem_ditongo(palavra)[0]:
             return True
-        if tem_gu_qu(palavra):
+        if tem_gu_qu(palavra)[0]:
             return True
-        if tem_tritongo_formato(palavra):
+        if tem_tritongo(palavra)[0]:
             return True
-        if tem_hiato_vogais_duplas_formato(palavra):
+        if tem_hiato_vogais_duplas(palavra)[0]:
             return True
-        if tem_bcdfgptv_lh_formato(palavra):
+        if tem_consoante_indivisivel(palavra)[0]:
             return True
-        if tem_outros_formatos(palavra):
+        if tem_outros_formatos(palavra)[0]:
             return True
+ 
     return False
+
+# Função que retorna uma determinada palavra dividida em sílabas
+def separa_formato(palavra,formato,silabas,aux):
+    count = 0
+    for i in formato:
+        if i != '-':
+            aux += palavra[count]
+            count += 1
+        else:
+            silabas.append(aux)
+            aux = ""
+    silabas.append(aux)
+
+def separa_em_silabas(palavra):
+    silabas = []
+    aux = ""
+    t = len(palavra)
+    if canonicidade(palavra):
+        for i in range(t):
+            if i % 2 != 0:
+                aux += palavra[i]
+                silabas.append(aux)
+            else:
+                aux = palavra[i]
+    else:
+        # Separa palavras com tritongo
+        finder = tem_tritongo(palavra)
+        if finder[0]:
+            separa_formato(palavra,formatos_tritongo_hifenizados[finder[1]],silabas,aux)
+            return silabas
+        # Separa palavras com ditongo
+        finder = tem_ditongo(palavra)
+        if finder[0]:
+            separa_formato(palavra,formatos_ditongo_hifenizados[finder[1]],silabas,aux)
+            return silabas
+        # Separa palavras com hiatos ou vogais duplas
+        finder = tem_hiato_vogais_duplas(palavra)
+        if finder[0]:
+            separa_formato(palavra,formatos_hiatos_vogais_duplas_hifenizados[finder[1]],silabas,aux)
+            return silabas
+        # Separa palavras com qu ou gu
+        finder = tem_gu_qu(palavra)
+        if finder[0]:
+            separa_formato(palavra,formatos_gu_qu_hifenizados[finder[1]],silabas,aux)
+            return silabas
+        # Separa palavras com consoantes indivisíveis
+        finder = tem_consoante_indivisivel(palavra)
+        if finder[0]:
+            separa_formato(palavra,formatos_consoante_indivisivel_hifenizados[finder[1]],silabas,aux)
+            return silabas
+        # Separa palavras com cz ou ps
+        finder = tem_cz_ps(palavra)
+        if finder[0]:
+            separa_formato(palavra,formatos_cz_ps_hifenizados[finder[1]],silabas,aux)
+            return silabas
+        # Separa outros formatos
+        finder = tem_outros_formatos(palavra)
+        if finder[0]:
+            separa_formato(palavra,outros_formatos_hifenizados[finder[1]],silabas,aux)
+            return silabas 
+    return silabas
